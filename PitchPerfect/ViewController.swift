546e4382599296a0f43a38347b20cd0995a5c7ae
Jimmy Brown <jabro4@gmail.com>
2016-03-20 16:52:32 -0700

    Initial commit of Udacity PitchPerfect app.

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(sender: AnyObject) {
+        print("record button pressed")
+        recordingLabel.text = "Recording in progress"
+    }
 
+    @IBAction func stopRecording(sender: AnyObject) {
+        print("stop recording button pressed")
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        print("viewWillAppear called")
+    }
 }
 
